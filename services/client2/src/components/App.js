6372c550dbbd14c0a4f029e18c86aea65a9e5a5f
Tyler <tylerschauer@gmail.com>
2019-03-16 19:52:23 -0400

    logout working

--- services/client2/src/components/App.js
+++ services/client2/src/components/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import MainWindow from './main/MainWindow';
 import Login from './Login';
 import RecipeList from './RecipeList';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import protectedFetch from './helpers';
+import { UserContext } from './stores/UserStore';
 
 
 
@@ -16,76 +17,18 @@ function ComingSoon() {
 }
 
 
+
+function Logoff() {
+  const { setLogin } = useContext(UserContext);
+  localStorage.removeItem('Authorization');
+  setLogin(false);
+
+  return (
+    <Redirect to="/" />
+  )
+}
+
 export default function App(props) {
-  // const [recipes, setRecipe] = useState([]);
-  // const [currentRecipe, setCurrent] = useState(0);
-  // const [loggedIn, setLogin] = useState(false);
-  // const [userRecipes, setUserRecipes] = useState([]);
-  // const [userRecipeList, setUserRecipeList] = useState([]);
-  //const [firstLoad, setFirstLoad] = useState(true);
-  // const numRecipesToFetch = 5;
-  //const userUrlPrefix = "http://localhost:5003/";
-  // const recipeUrlPrefix = "http://localhost:5000/"
-
-
-  // useEffect(() => {
-  //   // fetch starting recipes
-  //   if (firstLoad){
-  //   // fetchRandomRecipes(numRecipesToFetch);
-  //   if (localStorage.getItem('Authorization') !== null) {
-  //     // test if login token still ok and renew
-  //     checkLoggedIn();
-  //   }
-  //   //fetchRecipeLists();
-  //   }
-  //
-  //   setFirstLoad(false);
-  // })
-
-  // const checkLoggedIn = async () => {
-  //   console.log('checking for login')
-  //   const url = userUrlPrefix + 'users/check-login';
-  //   console.log(url);
-  //   const response = await protectedFetch(url, 'GET');
-  //   if (response.loggedIn === true) {
-  //     console.log("user is logged in");
-  //     setLogin(true);
-  //     localStorage.setItem('Authorization', response.token);
-  //   }
-  // }
-
-
-  // const fetchRandomRecipes = async (numRecipes) => {
-  //   const url = recipeUrlPrefix + `recipes/random/${numRecipes}`;
-  //   console.log(url);
-  //   const response = await fetch(
-  //     url
-  //   );
-  //   const data = await response.json();
-  //   setRecipe([...recipes,...data.recipes])
-  // }
-  //
-  // const cycleRecipes = () => {
-  //   // fetches new recipes when needed
-  //   setCurrent(currentRecipe + 1);
-  //   if(currentRecipe + 2 === recipes.length) {
-  //     fetchRandomRecipes(numRecipesToFetch);
-  //   }
-  // }
-
-  // const addRecipe = () => {
-  //   if (!userRecipes.includes(recipes[currentRecipe])) {
-  //     setUserRecipes([...userRecipes, recipes[currentRecipe]]);
-  //     console.log(userRecipes);
-  //   }
-  // }
-  // const fetchRecipeLists = async () => {
-  //   const url = userUrlPrefix + 'users/get-recipeLists';
-  //   const response = await protectedFetch(url,'GET')
-  //   if (response.recipeList != undefined){
-  //     setUserRecipeList(response.recipeList);
-  //   }
-  // }
 
 
   return (
@@ -104,7 +47,7 @@ export default function App(props) {
         <Route path="/grocery-list/" component={ComingSoon} />
         <Route path="/user/friends/" component={ComingSoon} />
         <Route path="/search/filters/" component={ComingSoon} />
-
+        <Route path="/users/logoff/" component={Logoff} />
     </div>
   )
 }
--- services/client2/src/components/stores/UserStore.js
+++ services/client2/src/components/stores/UserStore.js
@@ -40,6 +40,12 @@ export default function User(props) {
     if (loggedIn === true) {
       fetchRecipeLists();
     }
+    else {
+      setUserRecipes([]);
+      setUserRecipeList([]);
+      setSavedRecipes([]);
+
+    }
   }, [loggedIn])
 
   // async function that fetches user's recipe lists
